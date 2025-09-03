7ad7abc29e901cc354bda7143863b821e528633c
mmuopm <mulyish@gmail.com>
2025-09-03 15:42:36 +0300

    библиотека начало

--- WebBooks/catalog/admin.py
+++ WebBooks/catalog/admin.py
@@ -2,11 +2,22 @@ from django.contrib import admin
 
 from .models import Author, Book, Genre, Language, Publisher, Status, BookInstance
 
-admin.site.register(Author)
-admin.site.register(Book)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'photo')
+admin.site.register(Author, AuthorAdmin)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
+
+#admin.site.register(Book)
+#admin.site.register(BookInstance)
+
 admin.site.register(Genre)
 admin.site.register(Language)
 admin.site.register(Publisher)
 admin.site.register(Status)
-admin.site.register(BookInstance)
+
 

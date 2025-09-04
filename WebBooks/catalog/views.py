1a9da865639fa36999533b5a7daa90eac36bd1fd
mmuopm <mulyish@gmail.com>
2025-09-04 12:59:51 +0300

    второе

--- WebBooks/catalog/views.py
+++ WebBooks/catalog/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Book, BookInstance
+from .models import Book, BookInstance, Author
 
 
 def index(request):
@@ -9,6 +9,15 @@ def index(request):
     num_books = Book.objects.all().count()
     num_instance = BookInstance.objects.all().count()
     num_instance_available = BookInstance.objects.filter(status_exact=2).count()
+
+    author = Author.objects
+    num_author = Author.objects.count()
+    context = {'text_head' : text_head,
+               'books': books, 'num_books': num_books,
+               'num_instance': num_instance,
+               'num_instance_available' : num_instance_available,
+               'author': author, 'num_author': num_author}
+
     text_body = 'Содержание блаблабла'
     context = {'text_head': text_head, 'text_body': text_body}
 
